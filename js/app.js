367deaf7ac0f4f5c9408b361461d2c6c8fdd639b
fbk-rena <gaby.ranarz@gmail.com>
2017-05-03 16:24:05 -0500

    funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ var imagen = document.getElementsByClassName("imagen");
+ for(var i=0; i<imagen.length; i++){
+   imagen[i].addEventListener("click", mostrarImagen)
+ }
+
+ function mostrarImagen(){
+   var padre= document.getElementById("work");
+   var div = document.createElement("div");
+   div.id="imagenGrande";
+   div.setAttribute("class","contenedorImagenGrande");
+   var imagen = document.createElement("img");
+   imagen.setAttribute("class","tamanoImagenGrande");
+   imagen.src = this.src;
+   var btnCerrar = document.createElement("button");
+   btnCerrar.textContent="X";
+   btnCerrar.id="btnCerrar";
+   btnCerrar.setAttribute("class", "btnCerrar");
+   btnCerrar.setAttribute("onclick","cerrarImagen()");
+   padre.appendChild(div);
+   div.appendChild(imagen);
+   div.appendChild(btnCerrar);
+ }
+
+ function cerrarImagen(){
+   var padre= document.getElementById("work");
+   var imagenGrande = document.getElementById("imagenGrande");
+   padre.removeChild(imagenGrande);
+
+ }
